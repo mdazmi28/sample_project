9ca0021f3a03e94aabd708a554ce9988112059d6
mdazmi28 <md.azmi.siddique@gmail.com>
2024-10-09 13:06:01 +0600

    below hero section done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import Auctions from '../components/Auctions';
 import BeforeSPAndAcce from '../components/BeforeSPAndAcce';
 import Classifieds from '../components/Classifieds';
 import HeroSection from '../components/HeroSection';
+import SaleCampAndDetaisDay from '../components/SaleCampAndDetaisDay';
 import ShopByCategory from '../components/ShopByCategory';
 
 const LandingPage = () => {
     return (
         <div className="container mx-auto flex flex-col gap-11">
             <HeroSection/>
+            <SaleCampAndDetaisDay/>
             <ShopByCategory />
             <BeforeSPAndAcce />
             <Auctions/>
