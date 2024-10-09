12ca7e35676ea0a1f060f703e94466e34828385d
mdazmi28 <md.azmi.siddique@gmail.com>
2024-10-09 16:29:16 +0600

    section done with timer

--- src/components/GetAnApple.jsx
+++ src/components/GetAnApple.jsx
@@ -8,13 +8,16 @@ const GetAnApple = () => {
             <div className='w-full flex gap-4'>
                 <div className="w-1/2 h-[250px] bg-gray-300"></div>
                 <div className="flex flex-col">
-
                     <div className="ml-10 mt-10">
-                    <h1 className="text-[40px] font-bold">Get an Apple</h1>
-                    <h1 className="text-gray-400">Hurry up and get upto 20% discount on selected Apple Devices</h1>
-                    <div>
-                        <Timer/>
-                    </div>
+                        <h1 className="text-[40px] font-bold">Get an Apple</h1>
+                        <h1 className="text-gray-400">Hurry up and get upto 20% discount on selected Apple Devices</h1>
+                        <div>
+                            <Timer />
+                        </div>
+                        <div className="bg-yellow-300 w-[122px] h-[40px] flex justify-center items-center font-bold rounded-lg">
+                            Go Shopping
+                        </div>
+
                     </div>
                 </div>
             </div>
