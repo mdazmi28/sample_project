4866edfa050905f2d4a371595fe433745aff3903
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-10-10 22:57:21 +0600

    2nd slide bar structure complete

--- src/components/MasterLayout.jsx
+++ src/components/MasterLayout.jsx
@@ -6,20 +6,20 @@ import NavBar from "./NavBar";
 const MasterLayout = (props) => {
     return (
         <div className="h-screen flex gap-0">
-            {/* Left */}
-            <div className="pl-4">
-                <Menu />
-            </div>
-            {/* Right */}
-            <div className="bg-[#F7F8FA] overflow-y-scroll hide-scrollbar flex flex-col -ml-[10px]">
-                <NavBar className="w-full" />
-                <div className="">
-                    {props.children}
-                </div>
-
-            </div>
-
+        {/* Left */}
+        <div className="pl-4 z-10"> {/* Add relative positioning to the parent */}
+            <Menu />
         </div>
+        
+        {/* Right */}
+        <div className="bg-[#F7F8FA] overflow-y-scroll hide-scrollbar flex flex-col -ml-[10px]">
+            <NavBar className="w-full" />
+            <div className="">
+                {props.children}
+            </div>
+        </div>
+    </div>
+    
     );
 };
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,84 +9,97 @@ const menuItems = [
                 label: "Home",
                 href: "/",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5,]
             },
             {
                 icon: "/images/teacher.png",
                 label: "Teachers",
                 href: "/list/teachers",
                 visible: ["admin", "teacher"],
-                submenus: ['a','b',1,2,3,4,5,6,7,8,9,0,7,7,4,3,33,2,]
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/student.png",
                 label: "Students",
                 href: "/list/students",
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/parent.png",
                 label: "Parents",
                 href: "/list/parents",
                 visible: ["admin", "teacher"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/subject.png",
                 label: "Subjects",
                 href: "/list/subjects",
                 visible: ["admin"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/class.png",
                 label: "Classes",
                 href: "/list/classes",
                 visible: ["admin", "teacher"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/lesson.png",
                 label: "Lessons",
                 href: "/list/lessons",
                 visible: ["admin", "teacher"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/exam.png",
                 label: "Exams",
                 href: "/list/exams",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/assignment.png",
                 label: "Assignments",
                 href: "/list/assignments",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/result.png",
                 label: "Results",
                 href: "/list/results",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/attendance.png",
                 label: "Attendance",
                 href: "/list/attendance",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/calendar.png",
                 label: "Events",
                 href: "/list/events",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/message.png",
                 label: "Messages",
                 href: "/list/messages",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7]
             },
             {
                 icon: "/images/announcement.png",
                 label: "Announcements",
                 href: "/list/announcements",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
         ],
     },
@@ -98,18 +111,21 @@ const menuItems = [
                 label: "Profile",
                 href: "/profile",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/setting.png",
                 label: "Settings",
                 href: "/settings",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
             {
                 icon: "/images/logout.png",
                 label: "Logout",
                 href: "/logout",
                 visible: ["admin", "teacher", "student", "parent"],
+                submenus: ['a', 'b', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7, 7, 4, 3, 33, 2,]
             },
         ],
     },
@@ -117,7 +133,7 @@ const menuItems = [
 
 const Menu = () => {
     const [isMenuVisible, setIsMenuVisible] = useState(false);
-    const [onMouse, setOnMouse] = useState(false);
+    const [hoveredMenuItem, setHoveredMenuItem] = useState(null);
 
     const toggleMenu = () => {
         setIsMenuVisible(!isMenuVisible);
@@ -125,16 +141,13 @@ const Menu = () => {
 
 
 
-    const mouseEnter = () =>{
-        setOnMouse(!onMouse)
-        console.log("Enter")
-        console.log(onMouse)
-    }
-    const mouseLeace = () =>{
-        console.log("Leave")
-        console.log(onMouse)
-        // setOnMouse(!onMouse)
-    }
+    const handleMouseEnter = (menuItem) => {
+        setHoveredMenuItem(menuItem);  // Set the hovered item
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredMenuItem(null);  // Reset when mouse leaves
+    };
 
     return (
         <div className="mt-4 text-sm w-[270px]">
@@ -153,29 +166,45 @@ const Menu = () => {
                     </div>
                 )
             }
-          
-            <div className="pt-4">
-                {menuItems.map((item, index) => (
-                    <div key={index} className="flex flex-col gap-2">
-                        {item.items.map((menuItem, index) => (
-                            <div
-                                key={index}
-                                href={menuItem.href}
-                                className="flex items-center justify-start gap-4 text-black py-2 md:px-2 rounded-md hover:bg-lamaSkyLight transition-all"
-                            >
-                                <img src={menuItem.icon} width={20} height={20} />
-                                <span onMouseEnter={mouseEnter} onMouseLeave={mouseLeace}
-                                    className={`transition-all duration-300 ease-in-out ${isMenuVisible
-                                        ? "inline-block w-auto opacity-100"
-                                        : "w-0 opacity-0 overflow-hidden"
-                                        }`}
+            <div className="flex">
+                <div className="pt-4">
+                    {menuItems.map((item, index) => (
+                        <div key={index} className="flex flex-col gap-2 w-[250px]">
+                            {item.items.map((menuItem, index) => (
+                                <div
+                                    key={index}
+                                    onMouseEnter={() => handleMouseEnter(menuItem)}  // Track hover event
+                                    // onMouseLeave={handleMouseLeave}
+                                    href={menuItem.href}
+                                    className="flex items-center justify-start border gap-4 text-black py-2 md:px-2 rounded-md hover:bg-lamaSkyLight transition-all"
                                 >
-                                    {menuItem.label}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
-                ))}
+                                    <img src={menuItem.icon} width={20} height={20} />
+                                    <span
+                                        className={`transition-all duration-300 ease-in-out ${isMenuVisible
+                                            ? "inline-block w-auto opacity-100"
+                                            : "w-0 opacity-0 overflow-hidden"
+                                            }`}
+                                    >
+                                        {menuItem.label}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+                
+                {/* Show submenus of hovered item */}
+                {
+                    hoveredMenuItem && (
+                        <div className="bg-white flex flex-col gap-4 p-10 border-l">
+                            {hoveredMenuItem.submenus.map((submenu, idx) => (
+                                <div key={idx} className="text-black" onMouseLeave={handleMouseLeave}>
+                                    {submenu}
+                                </div>
+                            ))}
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
